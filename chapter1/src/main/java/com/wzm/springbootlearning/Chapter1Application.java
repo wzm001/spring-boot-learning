2612af1d3ff376689fbb582307710c1ffd2ce7c4
wzm001 <wuzhiming@hotmail.com>
2018-05-12 13:07:17 +0800

    chapter 1

---
+++ chapter1/src/main/java/com/wzm/springbootlearning/Chapter1Application.java
@@ -0,0 +1,16 @@
+package com.wzm.springbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world of spring boot
+ *
+ */
+@SpringBootApplication
+public class Chapter1Application {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(Chapter1Application.class, args);
+    }
+}
---
+++ chapter1/src/main/java/com/wzm/springbootlearning/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.wzm.springbootlearning.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, Spring-boot!";
+    }
+}
